3fd3874a9b3bf94584ea3af72bea342c8fdb5710
okiceokano <57456536+okiceokano@users.noreply.github.com>
2021-09-29 09:31:12 +0200

    +day16

---
+++ ex06/src/index.js
@@ -0,0 +1,8 @@
+exports.firstName = (firstName) => {
+    return firstName.toUpperCase();
+}
+exports.lastName = (lastName) => {
+    return lastName.toLowerCase();
+}
+console.log(this.firstName("sulejman"));
+console.log(this.lastName("velicanstveni"));
